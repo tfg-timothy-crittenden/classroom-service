160b29f04a44b83410c8a50405424d4047299476
unknown <tim_critt@hotmail.com>
2026-08-31 19:11:36 +0200

    Updated flyway to fix mismatch with member/membership table name

--- src/main/resources/db/migration/V4__add_constraint_not_null_name_surname_member_table.sql
+++ src/main/resources/db/migration/V4__add_constraint_not_null_name_surname_member_table.sql
@@ -1,8 +1,8 @@
-ALTER TABLE member
+ALTER TABLE membership
     DROP CONSTRAINT IF EXISTS chk_member_name_not_blank,
     DROP CONSTRAINT IF EXISTS chk_member_surname_not_blank;
 
-ALTER TABLE member
+ALTER TABLE membership
     ADD CONSTRAINT chk_member_name_not_blank
         CHECK (TRIM(name) <> ''),
     ADD CONSTRAINT chk_member_surname_not_blank
