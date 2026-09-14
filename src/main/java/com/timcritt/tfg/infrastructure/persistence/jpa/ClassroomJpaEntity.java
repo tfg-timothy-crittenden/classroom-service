85860e2568e142d2a07fda84e5ee9af92e2f9056
unknown <tim_critt@hotmail.com>
2026-09-15 00:14:20 +0200

    Renamed PasswordResetToken.invalidate() to revoke()

--- src/main/java/com/timcritt/tfg/infrastructure/persistence/jpa/ClassroomJpaEntity.java
+++ src/main/java/com/timcritt/tfg/infrastructure/persistence/jpa/ClassroomJpaEntity.java
@@ -25,7 +25,7 @@ public class ClassroomJpaEntity {
     private Instant createdAt;
     @Column(nullable = false)
     private Instant updatedAt;
-    @Column(nullable = false, name = "join_code", length = 50)
+    @Column(name = "join_code", length = 50)
     private String joinCode;
 
     @OneToMany(
